4a59dd2089d00e2690b1fd4e49498826f578acb0
Izzet Mustafaiev <izzet@mustafaiev.com>
2022-12-31 17:50:13 +0000

    Types

--- src/demo.ts
+++ src/demo.ts
@@ -1,5 +1,32 @@
 class Contact {
-    id;
-    name;
-    birthDate;
+  id;
+  name;
+  birthDate;
 }
+
+let x: number;
+let y: string;
+
+type ContactName = string;
+interface IContact {
+  id: number;
+  name: ContactName;
+  birthDate?: Date;
+  status?: ContactStatus;
+  clone(contact: IContact): IContact;
+}
+
+enum ContactStatus {
+  Active = "A",
+  Inactive = "I",
+  New = "N",
+}
+
+let primaryContact: IContact = {
+  id: 1,
+  name: "Sam",
+  status: ContactStatus.New,
+  clone: function (contact): IContact {
+    return Object.apply({}, contact);
+  },
+};
