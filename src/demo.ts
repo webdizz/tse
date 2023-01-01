437bc1acbc168466693053436bfdb69a8fb6e278
Izzet Mustafaiev <izzet@mustafaiev.com>
2023-01-01 13:36:00 +0000

    Omit, Pick and Required

--- src/demo.ts
+++ src/demo.ts
@@ -47,3 +47,37 @@ getValue(primaryContact, "id");
 
 let ctx: Record<string, string | number> = { name: "Bruce Lee" };
 ctx.id = 1234;
+
+interface Query {
+  sort?: "asc" | "desc";
+  matches(val): boolean;
+}
+
+type ContactQuery = Omit<
+  Partial<Record<keyof IContact, Query>>,
+  "birthDate" | "status"
+>;
+
+type ContactPickQuery = Pick<
+  Partial<Record<keyof IContact, Query>>,
+  "id"|"name"
+>;
+
+type RequiredContactQuery = Required<ContactQuery>
+
+function searchContacts(
+  contacts: IContact[],
+  query: Record<keyof IContact, ContactQuery>
+) {
+  return contacts.filter((contact) => {
+    for (const property of Object.keys(contact)) {
+      const propertyQuery = query[property];
+      if (propertyQuery && propertyQuery.matches(contact[property])) {
+        return true;
+      }
+    }
+    return false;
+  });
+}
+
+conts filteredContacts = searchContacts([], {id:{matches:(id)=>id==123}})
